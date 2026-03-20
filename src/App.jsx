13e5442d6e00909aea76ec6ea28aac12450fcd9e
Meggyis <katalinsimon1413@gmail.com>
2026-03-20 14:56:03 +0100

    Added the GitHub repo URL

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ import './App.css'
 export default function App() {
 
   return ( 
-  <div className='App'>Weather App</div>
+  <div className='App'>
+    Weather App
+    <footer>
+      This project was coded by Katalin Simon and is open-sourced on GitHub.
+      <br />
+    <a href="https://github.com/Meggyis/react-weather-app" target='_blank'>
+    View on GitHub</a> 
+    </footer>
+    </div>
+   
   )
 }
 
