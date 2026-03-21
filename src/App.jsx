5406462ceee1c81b8e570ff442cc473ddc58b8d0
Meggyis <katalinsimon1413@gmail.com>
2026-03-21 10:33:04 +0100

    Added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ export default function App() {
 
   return ( 
   <div className='App'>
-    Weather App
+    <div className='container'>
+   <h1>Weather App</h1>
     <footer>
       This project was coded by Katalin Simon and is open-sourced on GitHub.
       <br />
     <a href="https://github.com/Meggyis/react-weather-app" target='_blank'>
     View on GitHub</a> 
+   
     </footer>
+     </div>
     </div>
    
   )
